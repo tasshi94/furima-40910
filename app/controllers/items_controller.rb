224f69a1c3fd0ec2dca8290f4addd4d5426aafb6
tasshi94 <ytashiro94mc@gmail.com>
2024-10-06 06:22:44 +0900

    newアクション、createアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to '/'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :explanation, :category_id, :condition_id, :shipping_fee_id, :shipping_from_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
 end
