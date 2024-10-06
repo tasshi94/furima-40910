51f5e385191f866a0c60b9eff91016a1bd04e237
tasshi94 <ytashiro94mc@gmail.com>
2024-10-06 13:43:51 +0900

    販売手数料・販売利益の計算結果を表示させる実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,20 @@
+const price = () => {
+
+const priceInput = document.getElementById("item-price");
+const addTaxDom = document.getElementById("add-tax-price");
+const addProfitDom = document.getElementById("profit");
+
+priceInput.addEventListener("input", () => {
+  const inputValue = parseFloat(priceInput.value);
+  console.log(inputValue);
+
+  const taxAmount = Math.floor(inputValue * 0.1);
+  const profitAmount = Math.floor(inputValue - taxAmount);
+
+  addTaxDom.innerHTML = taxAmount;
+  addProfitDom.innerHTML = profitAmount;
+})
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
