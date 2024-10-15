d65937ff3f5a0172da805c19527f239cffd66030
tasshi94 <ytashiro94mc@gmail.com>
2024-10-15 23:56:57 +0900

    商品一覧、詳細、編集機能追加実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,9 +23,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    return if current_user == @item.user
-
-    redirect_to root_path
+    if current_user != @item.user || @item.order.present?
+      redirect_to root_path
+    end
   end
 
   def update
